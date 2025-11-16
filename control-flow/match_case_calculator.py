8fe776829a8438ee033df16e79915bb6e1f7244f
Hajar <hajar.elhaj20@gmail.com>
2025-11-16 23:03:40 +0100

    Weather Recommendation Program

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,18 +1,22 @@
-num1 = int(input("Enter the first number:"))
-num2 = int(input("Enter the second number:"))
-operation_type = input("Choose the operation (+, -, *, /):")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operation_type = input("Choose the operation (+, -, *, /): ")
+
 match operation_type:
     case "+":
-        print(f"The sum is: {num1 + num2}")
+        result = num1 + num2
+        print(f"The result is {result}.")
     case "-":
-        print(f"The difference is: {num1 - num2}")
+        result = num1 - num2
+        print(f"The result is {result}.")
     case "*":
-        print(f"The product is: {num1 * num2}")
+        result = num1 * num2
+        print(f"The result is {result}.")
     case "/":
         if num2 != 0:
-            print(f"The quotient is: {num1 / num2}")
+            result = num1 / num2
+            print(f"The result is {result}.")
         else:
             print("Cannot divide by zero.")
     case _:
         print("Invalid operation selected.")
-print("Thank you for using the calculator!")
