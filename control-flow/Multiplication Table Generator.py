22699283df1abfc2d1ea3076a7312b4d90c083e7
Hajar <hajar.elhaj20@gmail.com>
2025-11-16 23:32:59 +0100

    Delete control-flow/Multiplication Table Generator.py

--- control-flow/Multiplication Table Generator.py
+++
@@ -1,7 +0,0 @@
-number = int(input("Enter a number to see its multiplication table: "))
-result = 0
-for i in range(1, 11):
-    result = number * i
-    print(f"{number} * {i} = {result}")
-    i+=1
-    print()
