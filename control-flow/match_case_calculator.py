7806c393e547cd2ee50ff16b9c356610cafb3c7e
Hajar <hajar.elhaj20@gmail.com>
2025-11-16 22:16:06 +0100

    Simple Calculator with Match Case

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,18 @@
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+operation_type = input("Choose the operation (+, -, *, /):")
+match operation_type:
+    case "+":
+        print(f"The sum is: {num1 + num2}")
+    case "-":
+        print(f"The difference is: {num1 - num2}")
+    case "*":
+        print(f"The product is: {num1 * num2}")
+    case "/":
+        if num2 != 0:
+            print(f"The quotient is: {num1 / num2}")
+        else:
+            print("Error: Division by zero is not allowed.")
+    case _:
+        print("Invalid operation selected.")
+print("Thank you for using the calculator!")
