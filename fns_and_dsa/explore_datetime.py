3050e4647dfa56729cb2f5b23b3cd746a0ceaaf0
Hajar <hajar.elhaj20@gmail.com>
2025-11-22 15:10:36 +0100

    Explore  Module

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -4,13 +4,13 @@ def display_current_datetime():
     current_date = datetime.now()
     formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
     print("Current date and time:", formatted_date)
-    return current_date
+    return formatted_date
 
 def calculate_future_date(days):
     future_date = datetime.now() + timedelta(days=days)
     formatted_future = future_date.strftime("%Y-%m-%d")
     print("Future date:", formatted_future)
-    return future_date
+    return formatted_future
 
 def main():
    
