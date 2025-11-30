4bfcfdc5a6757ee6efad16b4e153b5508d88bf5f
Hajar <hajar.elhaj20@gmail.com>
2025-11-30 19:19:30 +0100

    Implementing Basic OOP for a Library Management System

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,59 @@
+class Book:
+    """Represents a book with a title, author, and availability status."""
+
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False  # Private attribute to track availability
+
+    def check_out(self):
+        """Mark the book as checked out if it's available."""
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        """Mark the book as returned (available)."""
+        self._is_checked_out = False
+
+    def is_available(self):
+        """Return True if the book is available, False if checked out."""
+        return not self._is_checked_out
+
+    def __str__(self):
+        """Return a string representation of the book."""
+        return f"{self.title} by {self.author}"
+
+
+class Library:
+    """Represents a library managing a collection of books."""
+
+    def __init__(self):
+        self._books = []  # Private list of Book instances
+
+    def add_book(self, book):
+        """Add a Book instance to the library."""
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        """Check out a book by title if it exists and is available."""
+        for book in self._books:
+            if book.title == title and book.is_available():
+                book.check_out()
+                return True
+        return False
+
+    def return_book(self, title):
+        """Return a book by title."""
+        for book in self._books:
+            if book.title == title:
+                book.return_book()
+                return True
+        return False
+
+    def list_available_books(self):
+        """Print all available books in the library."""
+        for book in self._books:
+            if book.is_available():
+                print(book)
