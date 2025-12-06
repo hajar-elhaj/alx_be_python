5028f5e5b7489809785e5583cfa518fbb883649a
Hajar <hajar.elhaj20@gmail.com>
2025-12-06 01:42:42 +0100

    Exploring Polymorphism and Method Overriding

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -1,14 +1,16 @@
 import math
 class Shape:
-    def area(self):
-        raise NotImplementedError
-
-class Rectangle(Shape):
     def __init__(self,width,length):
         self.width = width
         self.length = length
     def area(self):
-        return self.width * self.length
+        raise NotImplementedError
+
+class Rectangle(Shape):
+    def __init__(self, width, length):
+        super().__init__(width, length)
+    def area(self):
+        return self.length * self.width
 
 class Circle(Shape):
     def __init__(self, radius):
